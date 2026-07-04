5bf38d2e92354b8c015ac4a41ab808a6842f4277
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-07-04 17:33:25 +0600

    Добавлена вьюшка детального обзора темы

--- source/threads/views/thread.py
+++ source/threads/views/thread.py
@@ -1,13 +1,14 @@
 from urllib.parse import urlencode
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 from django.db.models import Q, Count
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView
 
-from threads.forms import SimpleSearchForm, ThreadForm
+from threads.forms import SimpleSearchForm, ThreadForm, ResponseForm
 from threads.models.thread import Thread
 
 THREADS_PAGE_SIZE = 10
@@ -69,3 +70,49 @@ class ThreadCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+class ThreadDetailView(LoginRequiredMixin, DetailView):
+    model = Thread
+    template_name = "threads/thread_detail.html"
+    context_object_name = "thread"
+    form_class = ResponseForm
+
+    def get_queryset(self):
+        return Thread.objects.select_related('author')
+
+    def get_success_url(self):
+        return reverse_lazy('threads:thread_detail', kwargs={'pk': self.object.pk})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = context.get('form') or self.get_form()
+
+        responses_qs = self.object.responses.select_related('author').annotate(
+            author_msg_count=Count('author__responses', distinct=True)
+        ).order_by('created_at')
+
+        paginator = Paginator(responses_qs, RESPONSES_PAGE_SIZE, orphans=1)
+        page_number = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        context['page_obj'] = page_obj
+        context['paginator'] = paginator
+        context['responses'] = page_obj.object_list
+        context['is_paginated'] = page_obj.has_other_pages()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+
+        if not request.user.is_authenticated:
+            return redirect(f"{reverse_lazy('accounts:login')}?next={request.path}")
+
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        return self.form_invalid(form)
+
+    def form_valid(self, form):
+        form.instance.thread = self.object
+        form.instance.author = self.request.user
+        form.save()
+        return super().form_valid(form)
