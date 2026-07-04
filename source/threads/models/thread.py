e6b16ea23f260e4271dcf068f655921cedd09869
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-07-04 15:53:00 +0600

    Добавлено поле содержимое в модель Темы

--- source/threads/models/thread.py
+++ source/threads/models/thread.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 
 class Thread(models.Model):
     title = models.CharField(max_length=70, verbose_name='Название темы')
+    content = models.TextField(max_length=500, verbose_name='Содержимое темы')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     author = models.ForeignKey(
         get_user_model(),
