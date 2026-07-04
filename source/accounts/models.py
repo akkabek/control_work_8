bd5afb53affe30c7e3303027422afe7b175f6d05
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-07-04 13:49:51 +0600

    Добавлена модель кастомного User

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    def get_avatar_path(instance, filename):
+        return f'avatars/{instance.username}/{filename}'
+
+    avatar = models.ImageField(
+        upload_to=get_avatar_path,
+        verbose_name='Аватар'
+    )
