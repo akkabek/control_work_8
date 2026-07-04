8b464f4e0f9d3b7422da791414907a05497a6d0a
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-07-04 16:14:26 +0600

    Добавлен Url для ListView Тем

--- source/threads/urls.py
+++ source/threads/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-# from threads.views import
+from threads.views.thread import ThreadListView
 
 app_name = "threads"
 urlpatterns = [
-
+path('', ThreadListView.as_view(), name='thread_list'),
+path('threads/', ThreadListView.as_view(), name='thread_list')
 ]
