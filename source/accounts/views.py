35d7fd0a490fe7c019f78ebeb4c01c75f79f7e33
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-07-04 18:24:56 +0600

    Добавлен профиль

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,13 +1,17 @@
 from django.contrib.auth import get_user_model, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
+from django.db.models import Count
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.views.generic import CreateView, DetailView
 
 from accounts.forms import MyUserCreationForm
+from threads.models.thread import Thread
 
 User = get_user_model()
+
+THREADS_PAGE_SIZE = 10
 
 
 class RegisterView(CreateView):
@@ -29,3 +33,23 @@ class RegisterView(CreateView):
         if self.request.POST.get("next"):
             redirect_url = self.request.POST.get("next")
         return redirect_url
+
+class ProfileDetailView(DetailView):
+    model = User
+    template_name = "accounts/profile.html"
+    context_object_name = "profile_user"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['responses_count'] = self.object.responses.count()
+
+        threads_qs = Thread.objects.filter(author=self.object).annotate(
+            response_count=Count('responses', distinct=True)
+        ).order_by('-created_at')
+        paginator = Paginator(threads_qs, THREADS_PAGE_SIZE, orphans=1)
+        page_obj = paginator.get_page(self.request.GET.get('page'))
+
+        context['page_obj'] = page_obj
+        context['threads'] = page_obj.object_list
+        context['is_paginated'] = page_obj.has_other_pages()
+        return context
