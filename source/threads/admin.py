d0c5d14fdad91f99773cf13e7bf81b56cc4b7ac7
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-07-04 18:20:28 +0600

    Добавлена админка

--- source/threads/admin.py
+++ source/threads/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from threads.models.thread import Thread
+from threads.models.response import Response
+
+
+@admin.register(Thread)
+class ThreadAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')
+    list_filter = ('author',)
+    search_fields = ('title', 'content')
+
+
+@admin.register(Response)
+class ResponseAdmin(admin.ModelAdmin):
+    list_display = ('thread', 'author', 'created_at')
+    list_filter = ('author', 'thread')
