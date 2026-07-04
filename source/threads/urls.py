6a4077e02085c3b29f14d194ef06d9bad8d7446a
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-07-04 17:34:36 +0600

    Detailview для Тем добавлен в Urls.py

--- source/threads/urls.py
+++ source/threads/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from threads.views.thread import ThreadListView
+from threads.views.thread import ThreadListView, ThreadCreateView, ThreadDetailView
 
 app_name = "threads"
 urlpatterns = [
-path('', ThreadListView.as_view(), name='thread_list'),
-path('threads/', ThreadListView.as_view(), name='thread_list')
+    path('', ThreadListView.as_view(), name='thread_list'),
+    path('threads/', ThreadListView.as_view(), name='thread_list'),
+    path('threads/create/', ThreadCreateView.as_view(), name='thread_create'),
+    path('threads/<int:pk>/', ThreadDetailView.as_view(), name='thread_detail'),
 ]
