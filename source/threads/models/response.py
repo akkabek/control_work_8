56509b2f69ba7e7eb5afbd4912665bcc7fd4def3
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-07-04 15:44:44 +0600

    Добавлена модель ответов в теме

--- source/threads/models/response.py
+++ source/threads/models/response.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from threads.models.thread import Thread
 
-# Create your models here.
+
+class Response(models.Model):
+    description = models.TextField(null=True, blank=True, max_length=500, verbose_name='Текст ответа')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.RESTRICT,
+        related_name="responses",
+        verbose_name="Автор"
+    )
+    thread = models.ForeignKey(
+        Thread,
+        on_delete=models.RESTRICT,
+        related_name="responses",
+        verbose_name="Тема"
+    )
