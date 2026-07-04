8faca0d7db10e7f2e82e4f626a5e37166c1f45e2
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-07-04 15:45:19 +0600

    Добавлена модель Темы форума

---
+++ source/threads/models/thread.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+class Thread(models.Model):
+    title = models.CharField(max_length=70, verbose_name='Название темы')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.RESTRICT,
+        related_name="threads",
+        verbose_name="Автор"
+    )
