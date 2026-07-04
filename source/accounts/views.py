eee0880cbbac16fcc6b358a4873fce7630699f44
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-07-04 17:49:51 +0600

    Реализована вьюшка для регистрации

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model, login
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views.generic import CreateView, DetailView
 
-# Create your views here.
+from accounts.forms import MyUserCreationForm
+
+User = get_user_model()
+
+
+class RegisterView(CreateView):
+    form_class = MyUserCreationForm
+    template_name = "accounts/registration.html"
+    model = User
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        redirect_url = reverse("threads:thread_list")
+
+        if self.request.GET.get("next"):
+            redirect_url = self.request.GET.get("next")
+
+        if self.request.POST.get("next"):
+            redirect_url = self.request.POST.get("next")
+        return redirect_url
