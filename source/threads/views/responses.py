14f20b64e51089928916f739cec14231fdb5bf7c
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-07-04 18:07:09 +0600

    Добавлена вьюшка для удаления ответа

--- source/threads/views/responses.py
+++ source/threads/views/responses.py
@@ -14,3 +14,11 @@ class ResponseUpdateView(AuthorOrModeratorRequiredMixin, UpdateView):
 
     def get_success_url(self):
         return reverse_lazy('threads:thread_detail', kwargs={'pk': self.object.thread_id})
+
+class ResponseDeleteView(AuthorOrModeratorRequiredMixin, DeleteView):
+    model = Response
+    template_name = "threads/response_confirm_delete.html"
+    context_object_name = "response"
+
+    def get_success_url(self):
+        return reverse_lazy('threads:thread_detail', kwargs={'pk': self.object.thread_id})
