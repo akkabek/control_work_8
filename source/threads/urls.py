2dc9df5f063baaf49094d129338d70e107bed943
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-07-04 18:16:20 +0600

    Добавлены url для запросов

--- source/threads/urls.py
+++ source/threads/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from threads.views.thread import ThreadListView, ThreadCreateView, ThreadDetailView
+from threads.views import ResponseUpdateView, ResponseDeleteView
+from threads.views import ThreadListView, ThreadCreateView, ThreadDetailView
 
 app_name = "threads"
 urlpatterns = [
@@ -8,4 +9,7 @@ urlpatterns = [
     path('threads/', ThreadListView.as_view(), name='thread_list'),
     path('threads/create/', ThreadCreateView.as_view(), name='thread_create'),
     path('threads/<int:pk>/', ThreadDetailView.as_view(), name='thread_detail'),
+
+    path('responses/<int:pk>/edit/', ResponseUpdateView.as_view(), name='response_update'),
+    path('responses/<int:pk>/delete/', ResponseDeleteView.as_view(), name='response_delete'),
 ]
