e70a195894091cab99992edd5a89790376c77493
Bektur_Akkaziev <bektur@MacBook-Pro-Bektur.local>
2026-07-04 18:06:02 +0600

    Добавлена вьюшка для изменения ответа

---
+++ source/threads/views/responses.py
@@ -0,0 +1,16 @@
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView, DeleteView
+
+from threads.forms import ResponseForm
+from threads.mixins import AuthorOrModeratorRequiredMixin
+from threads.models.response import Response
+
+
+class ResponseUpdateView(AuthorOrModeratorRequiredMixin, UpdateView):
+    model = Response
+    form_class = ResponseForm
+    template_name = "threads/response_form.html"
+    context_object_name = "response"
+
+    def get_success_url(self):
+        return reverse_lazy('threads:thread_detail', kwargs={'pk': self.object.thread_id})
